dd8eff87996f750c4bc069de28ff616f39383d41
Aditya paul <adityapaul.dibbo25@gmail.com>
2023-11-06 03:41:13 +0600

    doing some crud operation

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000;
+
+//middlewire
+app.use(cors())
+app.use(express.json()) 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.exrbbd1.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //database collections
+    const database = client.db("JobQuester");
+    const jobscollection = database.collection("alljobs");
+
+    // alljobs + category + email
+
+    app.get('/jobs', async (req, res) => {
+        
+        let query= {}
+        const category= req.query.job_category;
+        const email= req.query.email;
+        if(category){
+            query.job_category = category
+        }
+        else if(email){
+            query.email = req.query.email
+        }
+
+        const cursor = jobscollection.find(query);
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    
+    app.post("/jobs", async (req, res) => {
+        const query = req.body;
+        console.log(query)
+        const result = await jobscollection.insertOne(query)
+        res.send(result)
+    })
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Job Quester running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
